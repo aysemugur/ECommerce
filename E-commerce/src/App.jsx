f0c750a9c79b2cf90f9717da0bf6a3ad2e59aecd
aysemugur <aysemugr@gmail.com>
2024-11-03 17:49:17 +0300

    all depencies installed

---
+++ E-commerce/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-red-600">Merhaba Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
